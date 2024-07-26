a6a68e2c5de69328ed18413d872b2067fd31d4c3
benmath54 <159335972+benmath54@users.noreply.github.com>
2024-07-26 08:51:51 -0400

    Add files via upload

--- leastsquares.py
+++ leastsquares.py
@@ -6,13 +6,14 @@ Created on Tue Jun 11 10:32:12 2024
 """
 
 import numpy as np
-#from math import pi
+from math import pi
 from math import cos
 from math import sin
+from math import sqrt
 import random
 import matplotlib.pyplot as plt
 import statistics
-from scipy.stats import norm
+from scipy.optimize import minimize
 
 #Requires: the coffients and x value of a quadratic fuction
 #Modifies: Nothing
@@ -135,13 +136,46 @@ def derivative_a3(a,x):
     x = x[0]
     return float(-1*a[1]*sin(a[2]*x+a[3]))
 
+def chi_square(a, t, return_grad=True):
+    x = t[0]
+    y = t[1]
+    n = len(x)
+    
+    chi = 0
+    
+    # Computing chi-square
+    for i in range(n):
+        chi += (y[i] - non_lin_func(a, x[i]))**2
+    
+    # Computing gradients
+    d_a0 = 0
+    d_a1 = 0
+    d_a2 = 0
+    d_a3 = 0
+    
+    for i in range(n):
+        term = cos(a[2]*x[i] + a[3])
+        d_a0 += -2 * (-a[0] + y[i] + a[1] * term)
+        d_a1 += 2 * term * (term * a[1] + y[i] - a[0])
+        d_a2 += -2 * a[1] * x[i] * sin(x[i] * a[2] + a[3]) * (a[1] * term + y[i] - a[0])
+        d_a3 += -2 * a[1] * sin(a[3] + a[2] * x[i]) * (a[1] * term + y[i] - a[0])
+    
+    d_chi = np.array([d_a0, d_a1, d_a2, d_a3])
+    
+    # Returning results based on return_grad flag
+    if return_grad is False:
+        return chi
+    else:
+        return chi, d_chi
+
+
 def non_lin_xy_mat(a,m):
     x = random.uniform(-10,10)
     A_rb = np.array(x)
     R_rb = np.array(non_lin_func(a, x))
     
     for i in range(1,m):
-        x = random.uniform(-10,10)
+        x = random.uniform(0,4*pi)
         A_rn = np.array(x)
         A_rb = np.vstack((A_rb,A_rn))
         
@@ -170,19 +204,26 @@ def non_lin_starting_mat(t,m,a):
     
     return(A_rb,R_rb)
 
-m = 100
+m = 10
 a_int = ([1,1,1,1])
 
 xy = non_lin_xy_mat(a_int, m)
 
 error_array2 = np.empty(m)
 
+a0_array = np.empty(m)
+a1_array = np.empty(m)
+a2_array = np.empty(m)
+a3_array = np.empty(m)
+
 for i in range(1,m):
     xy_w_noise = func_noise(xy)
 
     delta_a = 1
 
-    current_a = ([0.75,0.75,0.75,0.75])
+    current_a = [0.75,0.75,0.75,0.75]
+    result = minimize(chi_square, current_a, jac=True, args=(xy_w_noise), method = 'CG')
+    current_a = result.x
     iterations = 0
     
     while (abs(delta_a) >= 0.05 or iterations  > 1000):
@@ -191,7 +232,7 @@ for i in range(1,m):
         beta = least_square(AR)
     
         a_hold = np.copy(current_a)
-    
+     
         current_a[0] = float(beta[0] +current_a[0])
         current_a[1] = float(beta[1] +current_a[1])
         current_a[2] = float(beta[2] +current_a[2])
@@ -199,14 +240,35 @@ for i in range(1,m):
         iterations+= 1
     
         delta_a = sum(current_a) - sum(a_hold)
-        
+    
+    a0_array[i] = current_a[0]
+    a1_array[i] = current_a[1]
+    a2_array[i] = current_a[2]
+    a3_array[i] = current_a[3]
     error = MSE_error(xy, current_a,True)
     error_array2[i] = error
     
 error_array2.sort()
+a0_array.sort()
+a1_array.sort()
+a2_array.sort()
+a3_array.sort()
+
+a0_std = statistics.stdev(a0_array)
+a1_std = statistics.stdev(a1_array)
+a2_std = statistics.stdev(a2_array)
+a3_std = statistics.stdev(a3_array)
+
+a0_mean = statistics.mean(a0_array)
+a1_mean = statistics.mean(a1_array)
+a2_mean = statistics.mean(a2_array)
+a3_mean = statistics.mean(a3_array)
+
+a0_ci = (a0_mean-1.96*(a0_std/sqrt(m)),a0_mean+1.96*(a0_std/sqrt(m)))
+a1_ci = (a1_mean-1.96*(a1_std/sqrt(m)),a1_mean+1.96*(a1_std/sqrt(m)))
+a2_ci = (a2_mean-1.96*(a2_std/sqrt(m)),a2_mean+1.96*(a2_std/sqrt(m)))
+a3_ci = (a3_mean-1.96*(a3_std/sqrt(m)),a3_mean+1.96*(a3_std/sqrt(m)))
 
 std_error2= statistics.stdev(error_array2)
 
 mean_error2 = statistics.mean(error_array2)
-
-plt.plot(error_array2)
